9597658fcc2514e39078c2338a9c84298ee74e77
Alejandro <armentaayalaalejandro@gmail.com>
2026-08-04 13:32:15 -0600

    added project

---
+++ Rationale.cpp
@@ -0,0 +1,45 @@
+/*
+
+    bird es el mas general
+
+flying bird     flyless bird
+
+eagle           penguin
+
+*/
+
+#include <iostream>
+
+// cuando son miembros tienen que ser el izquierdo
+
+class Rational {
+ public:
+  Rational(long a, long b) {
+    n_ = a;
+    d_ = b;
+  }
+  Rational& operator+=(const Rational& rhs);
+
+  std::ostream& operator<<(std::ostream& os);
+
+ private:
+  long n_;
+  long d_;
+};
+
+Rational& Rational::operator+=(const Rational& rhs) {
+  n_ = n_ * rhs.d_ + d_ * rhs.n_;
+  d_ = d_ * rhs.d_;
+  return *this;
+}
+
+std::ostream& Rational::operator<<(std::ostream& os) {
+  os << n_ << "/" << d_;
+  return os;
+}
+
+int main() {
+  Rational a(1, 2), b(1, 2);
+  a += b;
+  a << std::cout << "\n";
+}
